2a6b677f42381deb4551a14c4ea72cbc3c9c16b6
Christophe Beauregard <beauregardcp@gmail.com>
2018-03-22 17:45:14 -0400

    publish relay status when the button it pressed. Add a setting for the startup state of the relay rather than just turning it off.

--- esp8266Switch.ino
+++ esp8266Switch.ino
@@ -10,17 +10,20 @@ const int PIN_BUTTON = 13;
 HomieNode switchNode("plug", "switch");
 Button button1(PIN_BUTTON); // Connect your button between pin 2 and GND
 
+HomieSetting<bool> poweronState("plugon", "Default poweron state");
+
+static void setRelay(int state) {
+  digitalWrite(PIN_RELAY, state ? HIGH : LOW);
+  switchNode.setProperty("on").send(state ? "true" : "false");
+  Serial.print("Plug is ");
+  Serial.println(state ? "on" : "off");
+}
+
 bool lightOnHandler(HomieRange range, String value) {
   if (value == "true") {
-    digitalWrite(PIN_RELAY, HIGH);
-    Homie.setNodeProperty(switchNode, "on").send("true");
-    Serial.println("Light is on");
-  //  switchState = true;
+    setRelay(1);
   } else if (value == "false") {
-    digitalWrite(PIN_RELAY, LOW);
-    Homie.setNodeProperty(switchNode, "on").send("false");
-    Serial.println("Light is off");
-//    switchState = false;
+    setRelay(0);
   } else {
     Serial.print("Error Got: ");
     Serial.println(value);
@@ -30,29 +33,30 @@ bool lightOnHandler(HomieRange range, String value) {
   return true;
 }
 
-
-
 void setup() {
   Serial.begin(115200);
-  Serial.println();
+  Serial.println("ecoplug booting");
   Serial.println();
   //pinMode(PIN_BUTTON,INPUT_PULLUP);
   pinMode(PIN_RELAY, OUTPUT);
-  digitalWrite(PIN_RELAY, LOW);
+
   Homie.setLedPin(PIN_LED, LOW);
   //Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
   Homie_setFirmware("ecoplug", "1.0.0");
   switchNode.advertise("on").settable(lightOnHandler);
+
+  poweronState.setDefaultValue(false);
+  
   button1.begin();
   Homie.setup();
+
+  setRelay(poweronState.get());
 }
 
 void loop() {
   Homie.loop();
   if (button1.pressed())
   {
-    digitalWrite(PIN_RELAY, !digitalRead(PIN_RELAY));
+    setRelay( !digitalRead(PIN_RELAY) );
   }
-  
-
 }
