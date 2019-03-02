a4725232bfce66bd927b8bee214222e4b56e0152
Christophe Beauregard <beauregardcp@gmail.com>
2019-03-02 13:37:59 -0500

    Add optional support for WiOn 50055 led backlight on GPIO14.

--- esp8266Switch.ino
+++ esp8266Switch.ino
@@ -6,9 +6,11 @@ const int PIN_RELAY = 15;
 const int PIN_LED = 2;
 const int PIN_BUTTON = 13;
 
+// WiOn 50055 with dual USB and backlights
+const int PIN_BACKLIGHT = 14;
 
 HomieNode switchNode("plug", "switch");
-Button button1(PIN_BUTTON); // Connect your button between pin 2 and GND
+Button button1(PIN_BUTTON);
 
 HomieSetting<bool> poweronState("plugon", "Default poweron state");
 
@@ -33,17 +35,46 @@ bool lightOnHandler(HomieRange range, String value) {
   return true;
 }
 
+#ifdef PIN_BACKLIGHT
+HomieNode lightNode("backlight", "switch");
+static void setBacklight(int state) {
+  digitalWrite(PIN_BACKLIGHT, state ? HIGH : LOW);
+  lightNode.setProperty("on").send(state ? "true" : "false");
+  Serial.print("Backlight is ");
+  Serial.println(state ? "on" : "off");
+}
+
+bool backlightOnHandler(HomieRange range, String value) {
+  if (value == "true") {
+    setBacklight(1);
+  } else if (value == "false") {
+    setBacklight(0);
+  } else {
+    Serial.print("Error Got: ");
+    Serial.println(value);
+    return false;
+  }
+
+  return true;
+}
+#endif
+
 void setup() {
   Serial.begin(115200);
   Serial.println("ecoplug booting");
   Serial.println();
-  //pinMode(PIN_BUTTON,INPUT_PULLUP);
+  pinMode(PIN_BUTTON,INPUT_PULLUP);
   pinMode(PIN_RELAY, OUTPUT);
 
   Homie.setLedPin(PIN_LED, LOW);
-  //Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
-  Homie_setFirmware("ecoplug", "1.0.0");
+  Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
+  Homie_setFirmware("ecoplug", "1.0.1");
   switchNode.advertise("on").settable(lightOnHandler);
+
+#ifdef PIN_BACKLIGHT
+  pinMode(PIN_BACKLIGHT, OUTPUT);
+  lightNode.advertise("on").settable(backlightOnHandler);
+#endif
 
   poweronState.setDefaultValue(false);
   
